5ef28c88961d0398ba224366db72c763ee3bedb6
MindfulLearner <dimajosh1009@gmail.com>
2024-09-26 22:44:17 +0000

    love of coding

--- censura.php
+++ censura.php
@@ -1,4 +1,34 @@
-<?php
-echo "swag swag";
-?>
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>anima di drast</title>
+</head>
+<body>
+
+        <div>
+            <?php
+            $parolacce = ['swag', 'swagghissimo'];
+
+            function censura($contanitoreTesto, $parolacce)
+            {
+                foreach ($parolacce as $parolaccia) {
+                    $contanitoreTesto = str_ireplace($parolaccia, '***', $contanitoreTesto);
+                }
+                return $contanitoreTesto;
+            }
+
+            $nome = ($_POST['nome']);
+            $cognome = ($_POST['cognome']);
+
+            $nome = censura($nome, $parolacce);
+            $cognome = censura($cognome, $parolacce);
+
+            echo "<div>Nome: " . $nome . "</div>";
+            echo "<div>ognome: " . $cognome . "</div>";
+            ?>
+        </div>
+</body>
+</html>
 
---
+++ form.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="it">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP swag</title>
+  </head>
+  <body>
+    <h1>swagger</h1>
+    <h2>swag</h2>
+    <p>
+      <?php echo "ciao josh"; ?>
+    </p>
+    <form action="censura.php" method="POST">
+      <label for="nome">Nome:</label>
+      <input type="text" id="nome" name="nome" placeholder="NOME" required>
+      <label for="cognome">Cognome:</label>
+      <input type="text" id="cognome" name="cognome" placeholder="COGNOME" required>
+      <button type="submit">swagmanda</button>
+    </form>
+  </body>
+</html>
+
